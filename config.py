b2af5f40c86d7375afcd94a1b4a6f08ff3f38ec2
poojathote1991 <poojaghode6@gmail.com>
2023-07-07 16:47:46 +0530

    config file updates

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 PORT_NUMBER=8080
 MONGODB_PORT_NUMBER=27017
-path=r"static,model_file"
+path=r"static\model_file"
 MODEL_FILE_PATH=os.path.join(path,"Daimond_price_model.pkl")
 JSON_FILE_PATH=r'static\json_file\proj_data.json'
